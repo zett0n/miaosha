5d90c6fb2ecc1c08657b45f2b27a3b5a929f79c0
zett0n <d673326004@163.com>
2021-08-12 21:57:06 +0800

    新增war部署，修改sql文件

--- src/main/java/cn/edu/zjut/MiaoshaApplication.java
+++ src/main/java/cn/edu/zjut/MiaoshaApplication.java
@@ -3,13 +3,22 @@ package cn.edu.zjut;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("cn.edu.zjut.dao")
-public class MiaoshaApplication {
+// war包部署需要继承SpringBootServletInitializer
+public class MiaoshaApplication extends SpringBootServletInitializer {
+    // public class MiaoshaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MiaoshaApplication.class, args);
     }
 
+    // [war包部署]配置入口类是谁
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MiaoshaApplication.class);
+    }
 }
